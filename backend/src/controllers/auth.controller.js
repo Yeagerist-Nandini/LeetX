db862c988ed625b3608e1798c206f53967fc0bd5
Yeagerist-Nandini <nandinij324@gmail.com>
2025-04-28 18:39:11 +0530

    Auth controller completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -159,15 +159,10 @@ const login = asyncHandler(async (req, res) => {
 })
 
 
-const verifyEmail = asyncHandler(async (req, res) => {
-
-})
-
-
 const logout = asyncHandler(async (req, res) => {
     //clear the cookies to logout 
     await db.user.update({
-        where: { id: user.id },
+        where: { id: req.user.id },
         data: { refreshToken: null }
     })
 
@@ -177,38 +172,225 @@ const logout = asyncHandler(async (req, res) => {
     }
 
     return res
-           .status(200)
-           .clearCookie("accessToken", cookieOptions)
-           .clearCookie("refreshToken", cookieOptions)
-           .json(new ApiResponse(200, {}, "User logged Out"))
+        .status(200)
+        .clearCookie("accessToken", cookieOptions)
+        .clearCookie("refreshToken", cookieOptions)
+        .json(new ApiResponse(200, {}, "User logged Out"))
 })
 
 
-const refreshAccessToken = asyncHandler(async (req, res) => {
+const verifyEmail = asyncHandler(async (req, res) => {
+    //get token from url
+    const { token } = req.params;
 
+    //check if token is valid
+    const user = await db.user.findUnique({
+        where: {
+            emailVerificationToken: token,
+            emailVerificationExpriy: { gt: Date.now() }
+        }
+    })
+
+    if (!user) {
+        throw new ApiError(404, "Invalid verifiction token");
+    }
+
+    // verify user
+    const verifiedUser = await db.user.update({
+        where: { id: user.id },
+        data: {
+            emailVerificationToken: null,
+            emailVerificationExpiry: null,
+            isEmailVerified: true
+        }
+    })
+
+    if (!verifiedUser) {
+        throw new ApiError(500, "Email verification failed");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user.id, "Email verification successfull"));
 })
+
 
 const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const { password, new_password } = req.body;
 
+    //check if user is logged in 
+    const userId = req.user.id;
+
+    //get and check if user exists
+    let user = await db.user.findUnique({
+        where: { id: userId }
+    })
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+
+    // check if old password is matching
+    const isPasswordMatching = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatching) {
+        throw new ApiError(400, "Invalid password");
+    }
+
+    const newHashedPassword = await bcrypt.hash(new_password, 10);
+
+    //change the password
+    user = await db.user.update({
+        where: { id: userId },
+        data: { password: newHashedPassword }
+    });
+    if(!user) throw new ApiError(500, "Error while updating user password");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user, "Password changed successfully!"));
 })
+
 
 const forgotPasswordRequest = asyncHandler(async (req, res) => {
+    const userId = req.user.id;
 
+    //check if user exists
+    const user = await db.user.findUnique({
+        where: { id: userId }
+    })
+    if (!user) throw new ApiError(404, "User not found");
+
+    //generate reset password url
+    const token = cryto.randomBytes(32).toString("hex");
+    const resetPasswordUrl = `${process.env.BASE_URL}/api/v1/user/forgot-password/${token}`
+
+    //save it in db
+    const updated_user = await db.user.update({
+        where: { id: userId },
+        data: {
+            forgotPasswordToken: token,
+            forgotPasswordExpiry: Date.now() + (1000 * 60 * 20)
+        }
+    });
+    if (!updated_user) throw new ApiError(500, "Error while updating user");
+
+    //send it through mail
+    const mailOptions = {
+        email: user.email,
+        subject: "Reset Your Password",
+        mailgenContent: forgotPasswordMailgenContent(user.name, resetPasswordUrl)
+    }
+    sendEmail(mailOptions);
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, resetPasswordUrl, "Reset password link sent to your email."))
 })
 
-const resetForgottenPassword = asyncHandler(async (req, res) => {
 
+const resetForgottenPassword = asyncHandler(async (req, res) => {
+    //get token from url
+    const { token } = req.params;
+    if (!token) {
+        throw new ApiError(400, "Invalid token")
+    }
+
+    const { password } = req.body;
+
+    //check if token is valid
+    const user = await db.user.findUnique({
+        where: {
+            forgotPasswordToken: token,
+            forgotPasswordExpiry: { gt: Date.now() }
+        }
+    })
+
+    if (!user) {
+        throw new ApiError(404, "Invalid reset password token");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // update password
+    const updatedUser = await db.user.update({
+        where: { id: user.id },
+        data: {
+            forgotPasswordToken: null,
+            forgotPasswordExpiry: null,
+            password: hashedPassword
+        }
+    });
+
+    if (!updatedUser) {
+        throw new ApiError(500, "Email verification failed");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user.id, "New password created successfully"));
 })
 
 
 const getCurrentUser = asyncHandler(async (req, res) => {
+    const { userId } = req.user.id;
 
+    const user = await db.user.findUnique({
+        where: { id: userId },
+        select: {
+            email: true,
+            name: true,
+            image: true,
+            role: true
+        }
+    });
+    if (!user) throw new ApiError(404, "User not found");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user, "Fetched current user successfully"));
 })
 
 
-const resendEmailVerification = asyncHandler(async (req, res) => {
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    //get refresh token
+    const refreshToken = req.cookies.refreshToken || req.body.refreshToken;
+    if(!refreshToken) throw new ApiError(401, "unauthorized request");
 
+    // get payload from token
+    const decodedToken = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+    //check if user exists
+    const user = await db.user.findUnique({
+        where: { id: decodedToken.id }
+    });
+    if (!user) {
+        throw new ApiError(401, "Invalid refresh token")
+    }
+
+    //check if refresh token is expired 
+    if(refreshToken !== user.refreshToken){
+        throw new ApiError(401, "Refresh token is expired or used");
+    }
+
+    //get new accesstoken, refresh token 
+    const {accessToken, newRefreshToken} = await generateAccessAndRefereshTokens(user.id);
+
+    // save it in cookies
+    const cookieOptions = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+           .status(200)
+           .cookie("accessToken",accessToken, cookieOptions)
+           .cookie("refreshToken",newRefreshToken, cookieOptions)
+           .json(new ApiResponse(200, {accessToken, refreshToken: newRefreshToken}, "Access token refreshed"))
 })
+
+
+// const resendEmailVerification = asyncHandler(async (req, res) => {
+
+// })
 
 
 export {
@@ -219,7 +401,6 @@ export {
     logout,
     refreshAccessToken,
     register,
-    resendEmailVerification,
     resetForgottenPassword,
     verifyEmail,
 };  
---
+++ backend/src/validators/auth.validator.js
@@ -0,0 +1,70 @@
+import { body } from "express-validator";
+
+export const userRegistrationValidator = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty().withMessage("Email is required")
+            .isEmail().withMessage("Invalid Email"),
+        body("name")
+            .trim()
+            .notEmpty().withMessage("Name is required"),
+        body("password")
+            .trim()
+            .notEmpty().withMessage("Password is required")
+            .isLength({ min: 6 }).withMessage("password length should be at least 6 chars")
+            .isStrongPassword({
+                minLength: 6,
+                minLowercase: 1,
+                minNumbers: 1,
+                minUppercase: 1,
+                minSymbols: 1,
+            }).withMessage("please enter a strong password")
+    ]
+}
+
+
+export const userLoginValidator = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty().withMessage("Email is required")
+            .isEmail().withMessage("Email is not valid"),
+        body("password")
+            .trim()
+            .isEmpty().withMessage("Password is required"),
+    ];
+}
+
+export const resetPasswordValidator = () => {
+    return [
+        body("password")
+            .trim()
+            .isEmpty().withMessage("Password is required")
+            .isStrongPassword({
+                minLength: 6,
+                minLowercase: 1,
+                minUppercase: 1,
+                minSymbols: 1,
+                minNumbers: 1
+            }).withMessage("Please set a strong password")
+    ]
+}
+
+export const changePasswordValidator = () => {
+    return [
+        body("new_password")
+            .trim()
+            .isEmpty().withMessage("Password is required")
+            .isStrongPassword({
+                minLength: 6,
+                minLowercase: 1,
+                minUppercase: 1,
+                minSymbols: 1,
+                minNumbers: 1
+            }).withMessage("Please set a strong password"),
+        body("password")
+            .trim()
+            .isEmpty().withMessage("Password is required")
+    ]
+}
